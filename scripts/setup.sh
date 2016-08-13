e6bb14d5eb85777d3676059e8c864bd6cfc7e03e
Tobias Heinemann <tobias.heinemann@gmail.com>
2016-08-13 10:49:35 +0200

    Do a hard reset only when a temporary stash has been created

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,7 +9,7 @@ clone() {
     git config user.name $1
     lg="log --graph --pretty=format:'%C(auto)%h %s %C(red)%an'"
     git config alias.lg "$lg"
-    git config alias.up '!sha1=$(git stash create "autostash"); git reset --hard; git pull --rebase; [ -n "$sha1" ] && git stash apply "$sha1" && git gc --auto;'
+    git config alias.up '!sha1=$(git stash create "autostash"); [ -n "$sha1" ] && git reset --hard; git pull --rebase; [ -n "$sha1" ] && git stash apply "$sha1" && git gc --auto;'
     cd ..
 }
 
